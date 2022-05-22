a4b245192b3d45b4e5e22df84da2124b7f904e98
Rikardo Akosta <richy.andres.acosta@gmail.com>
2022-05-22 18:40:04 +0200

    se hacen modificaciones al contador y se empiza la a usar useEffect

--- src/App.js
+++ src/App.js
@@ -5,18 +5,33 @@ import Mensaje from './components/Mensaje';
 import Contraseña from './components/Contraseña';
 import Bombillo from './components/Bombillo';
 
+
+
+
+
 function App() {
 
+   /* desde aqui vamos a empezara usar useEffect  el cual se puede importar en la misma linea de codigo que usestate */
+   const colors = ["#845EC2", "#B39CD0", "#FBEAFF", "#00C9A7", "#C4FCEF"];//para buscar colores esta la pagina mycolor.space crea una buena ppaleta de colores
+ 
+   document.body.style = `background: ${colors[Math.floor(Math.random() * colors.length)]}`;
+
+   
 
   const [counter, setCounter] = useState(0);/* 0 es el valor desde donde queremos que parta,podria ser cualquier numero  */
 
   /* let counter = 8; :esta seria la forma en vanilla js*/
   
-  const increment = () => {
+  /* const increment = () => {
     setCounter(counter + 1);/* debe ser counter + 1 porque si ponemos counter++ estamos modificando el estado lo que se quiere es tener el estado counter y sumar 1 */
     /* counter = counter + 1;  :esta seria la forma en vanilla js */
-  }
-  const decrement = () => setCounter(counter - 1);
+  
+  /* const decrement = () => setCounter(counter - 1); */
+  //la parte anterior comentada desde donde empieza el lef es las diferentes fomas de hacer que funcione el copntador pero lo que sigue es la parte definitiva y va a estar en el return
+
+
+
+ 
 
 /* aqui vamos a poner la funcionalidad del componente bonbiñño */
   const [isOn, setIsOn] = useState(false);
@@ -28,28 +43,28 @@ function App() {
   return (
   <>
     <div className="App">
-      <h2>{counter }</h2>
-      <button onClick= {decrement}>
+      <h2>{counter}</h2>
+      <button onClick= {() => setCounter(counter -1)}>
         Decrementar
       </button>
-      <button onClick= {increment}>
+      <button onClick= {()=> setCounter(counter + 1)}>
         Incrementar
       </button>
     </div>
-      <br />
+     
     <div>
       <Mensaje />
-    <br />
+        <br />
       <Contraseña />
-    <br />
+        <br />
       <Bombillo isOn={isOn} changeIsOn={changeIsOn} />
-      <br />
+        <br />
       <Bombillo isOn={isOn} changeIsOn={changeIsOn}/>
-      <br />
-      <Bonbillo isOn={isOn} changeIsOn={changeIsOn}/>
-      <br />
+        <br />
       <Bombillo isOn={isOn} changeIsOn={changeIsOn}/>
-      <br />
+        <br />
+      <Bombillo isOn={isOn} changeIsOn={changeIsOn}/>
+      
     </div>
   </>
   );
